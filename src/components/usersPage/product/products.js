5988f8d5d3bcebd17c28de03ab7ab237aed635f6
Sardar Atabekov <sardarbekatabekov@gmail.com>
2019-12-04 15:34:57 +0600

    done select images in the product page

--- src/components/usersPage/product/products.js
+++ src/components/usersPage/product/products.js
@@ -8,7 +8,8 @@ export default class Product extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      select: 0
     };
   }
 
@@ -19,8 +20,30 @@ export default class Product extends React.Component {
   }
 
   render() {
-    let { data: product } = this.state;
+    let { data: product, select } = this.state;
+    console.log(select);
     console.log(product);
+    let images = [
+      {
+        url:
+          "https://static5.bolf.ua/rus_pm_%D0%9C%D1%83%D0%B6%D1%81%D0%BA%D0%B0%D1%8F-%D0%BA%D0%BE%D0%B6%D0%B0%D0%BD%D0%B0%D1%8F-%D0%BA%D1%83%D1%80%D1%82%D0%BA%D0%B0-%D1%87%D0%B5%D1%80%D0%BD%D0%B0%D1%8F-Bolf-1108-75138_5.jpg"
+      },
+      {
+        url:
+          "https://lp2.hm.com/hmgoepprod?set=source[/ea/f7/eaf72b0ad554090c75652ea73838da48651fd3b0.jpg],origin[dam],category[kids_girl14y_jumperscardigans_hoodiessweatshirts],type[DESCRIPTIVESTILLLIFE],res[m],hmver[1]&call=url[file:/product/main]"
+      },
+      {
+        url:
+          "https://lp2.hm.com/hmgoepprod?set=source[/03/11/0311b60dddf1397daa11a31d943f11b99671fe2b.jpg],origin[dam],category[ladies_hoodiesswetshirts_hoodies],type[DESCRIPTIVESTILLLIFE],res[m],hmver[1]&call=url[file:/product/main]"
+      },
+      {
+        url:
+          "https://i.pinimg.com/originals/a9/9d/ac/a99dac25d6b180ef38dd01b549b41c08.jpg"
+      },
+      {
+        url: "https://images.izi.ua/3309361"
+      }
+    ];
     return (
       <div className="category">
         <Header />
@@ -28,34 +51,17 @@ export default class Product extends React.Component {
           <div className="productPage">
             <div className="productImages">
               <div className="images">
-                <img
-                  alt={product.name}
-                  src="https://lmnop.kg/image/cache/catalog/Products1/MUJ%20Odejda/Kurtki/Li-Ning/AYMM085-3/123-800x600.jpg"
-                />
-                <img
-                  alt={product.name}
-                  src="https://lmnop.kg/image/cache/catalog/Products1/MUJ%20Odejda/Kurtki/Li-Ning/AYMM085-3/123-800x600.jpg"
-                />
-                <img
-                  alt={product.name}
-                  src="https://lmnop.kg/image/cache/catalog/Products1/MUJ%20Odejda/Kurtki/Li-Ning/AYMM085-3/123-800x600.jpg"
-                />
-                <img
-                  alt={product.name}
-                  src="https://lmnop.kg/image/cache/catalog/Products1/MUJ%20Odejda/Kurtki/Li-Ning/AYMM085-3/123-800x600.jpg"
-                />
+                {images.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image.url}
+                    alt={product.name}
+                    onClick={() => this.setState({ select: index })}
+                  />
+                ))}
               </div>
               <div className="mainImage">
-                {product.productInfos[0] &&
-                product.productInfos[0].images[0] &&
-                product.productInfos[0].images[0].url ? (
-                  <img
-                    src={product.productInfos[0].images[0].url}
-                    alt={product.name}
-                  />
-                ) : (
-                  "false"
-                )}
+                <img src={images[select].url} alt={product.name} />
               </div>
             </div>
             <div className="productInfo">
@@ -84,7 +90,12 @@ export default class Product extends React.Component {
                   </select>
                 </div>
               </div>
-              <button className="addBasket" onClick={()=>AddBasket(product.id)}>Добавить в корзину</button>
+              <button
+                className="addBasket"
+                onClick={() => AddBasket(product.id)}
+              >
+                Добавить в корзину
+              </button>
             </div>
             {/* {data &&
             data.map(product => (
