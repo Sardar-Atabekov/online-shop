aeb6228f7e96af6364bddfddf1793d0365c24a3f
Sardar Atabekov <sardarbekatabekov@gmail.com>
2019-12-12 17:38:05 +0600

    done login page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Categories from "./components/adminpage/categories/categories";
 import Products from "./components/adminpage/products/products";
 import AddProduct from "./components/adminpage/products/addProduct";
 import NotFound from "./components/404/404";
+import Login from "./components/loginpage/login";
 import Department from "./components/usersPage/derartament/departament";
 import Category from './components/usersPage/category/category';
 import Product from "./components/usersPage/product/products";
@@ -17,6 +18,7 @@ class App extends Component {
       <React.Fragment>
         <Switch>
           <Route exact path="/" component={MainPage} />
+          <Route exact path="/admin/" component={Login} />
           <Route exact path="/admin/departments" component={Departments} />
           <Route exact path="/admin/category" component={Categories} />
           <Route exact path="/admin/products" component={Products} />
---
+++ src/components/loginpage/login.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import { API } from "./../requests";
+import "./login.css";
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    let formData = new FormData(event.target),
+      data = {};
+
+    formData.forEach(function(value, key) {
+      data[key] = value;
+    });
+
+    let req = await fetch(`${API}/Account/Login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    const res = await req.json();
+    if (res.status === 400) {
+      console.log(res);
+      return this.setState({ error: res.message, status: true });
+    }
+    console.log(req);
+    if (res.access_token) {
+      localStorage.setItem("token", res.access_token);
+      localStorage.setItem("role", +res.role);
+      console.log(res.role);
+      if (+res.role === 1) {
+        this.props.history.push(`/meals`);
+      } else if (+res.role === 2) {
+        this.props.history.push(`/cook`);
+      } else if (+res.role === 4) {
+        this.props.history.push(`/barmen`);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="loginWrapper">
+        <div className="login">
+          <h1>E-SHOP</h1>
+          <form className="loginForm" onSubmit={this.handleSubmit}>
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Логин"
+              name="login"
+              required
+            />
+            <br />
+            <input
+              className="loginInput"
+              type="password"
+              placeholder="Пароль"
+              name="password"
+              required
+            />
+            <br />
+            {this.state.status ? (
+              <div className="errorMessage">{this.state.error}</div>
+            ) : null}
+            <br />
+            <input
+              className="loginInput loginBtn"
+              value="Sign in"
+              type="submit"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
