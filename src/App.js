61b7f68ff2112846a2b1112a4cb3ac3543b6e956
Ernest <exapl>
2019-12-21 19:19:43 +0600

    sardar

--- src/App.js
+++ src/App.js
@@ -3,33 +3,17 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Departments from "./components/adminpage/departments/departments";
 import Categories from "./components/adminpage/categories/categories";
-import Products from "./components/adminpage/products/products";
-import inDeveloping from "./inDeveloping.js";
-import Department from "./components/usersPage/derartament/departament";
-import Category from './components/usersPage/category/category';
-import Product from "./components/usersPage/product/products";
-// import ClothInfo from "./container/cloth-info";
-// import Cart from './container/card/';
+
+
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Switch>
-          {/* <Route exact path="/" component={Catalog} /> */}
-          <Route exact path="/admin/departments" component={Departments} />
+          <Route exact path="/" component={Departments} />
           <Route exact path="/admin/category" component={Categories} />
 
-          {/* <Route exact path="/catalog" component={ClothesPage} /> */}
-          {/* <Route exact path="/product/:id" component={ClothInfo} /> */}
-          {/* <Route exact path="/basket" component={Cart} /> */}
-          <Route exact path="/admin/products" component={Products} />
-          <Route exact path="/department/:id" component={Department} />
-          <Route exact path="/category/:id" component={Category} />
-          <Route exact path="/product/:id" component={Product} />
-
-
-          <Route exact path="*" component={inDeveloping} />
-        </Switch>
+          </Switch>
       </React.Fragment>
     );
   }
